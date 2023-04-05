b1fc9cc8a26e63be3dffe0a7bd3160a4fe185176
Aljith KJ <kwdeathlok@gmail.com>
2023-04-05 21:09:03 +0530

    Routes files created

--- server/index.js
+++ server/index.js
@@ -2,12 +2,20 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import connectDB from './Config/db.js';
+import classRouter from './Routes/classRouter.js';
+import schoolRouter from './Routes/schoolRouter.js';
+import studentRouter from './Routes/studentRouter.js';
+import teacherRouter from './Routes/teacherRouter.js';
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use('api/school', schoolRouter);
+app.use('api/student', studentRouter);
+app.use('api/class', classRouter);
+app.use('api/teacher', teacherRouter);
 
 
 app.get('/', (req, res) => {
